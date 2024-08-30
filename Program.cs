f477727110432065f483b352a8f9f7ac858b3be0
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-08-30 13:43:13 +0200

    uppgift if-satser

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+{
+    Console.Write("Är det fint väder?");
+string svar = Console.ReadLine();
+if(svar == "j" || svar == "J")
+    Console.WriteLine("Vi går på picknik");
+    else if (svar == "n" || svar == "N")
+    Console.WriteLine("Vi stannar inne och läser en bok");
+    else
+    Console.WriteLine("Jag förstår inte");
+} 
